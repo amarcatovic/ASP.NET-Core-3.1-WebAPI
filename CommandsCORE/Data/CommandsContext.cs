dc99555dca7834ffb120580789c31779cbf603e5
Amar Ćatović <amar_catovic@hotmail.com>
2020-07-25 12:58:03 +0200

    DbContext

---
+++ CommandsCORE/Data/CommandsContext.cs
@@ -0,0 +1,19 @@
+﻿using CommandsCORE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsCORE.Data
+{
+    public class CommandsContext : DbContext
+    {
+        public CommandsContext(DbContextOptions<CommandsContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
